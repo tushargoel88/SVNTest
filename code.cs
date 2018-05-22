0b9379756de9cefacf6ec1b4993c36b0a17e926c
Tushar <tushargoel88@gmail.com>
2018-05-22 10:58:51 +0530

    check in code file

---
+++ code.cs
@@ -0,0 +1,17 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 64E8-DEC5
+
+ Directory of C:\Users\Tushar\Desktop\GitHUb\SVNTest
+
+05/22/2018  10:58 AM    <DIR>          .
+05/22/2018  10:58 AM    <DIR>          ..
+05/22/2018  10:58 AM                 0 code.cs
+05/22/2018  12:55 AM               227 Jenkinsfile
+05/22/2018  01:03 AM               616 newfile.txt
+05/22/2018  12:55 AM                25 README.md
+05/22/2018  10:23 AM               671 SecondFile.txt
+05/22/2018  12:55 AM                14 SVN Testing.txt
+05/22/2018  12:55 AM                 2 test
+05/22/2018  12:55 AM                17 this.txt
+               8 File(s)          1,572 bytes
+               2 Dir(s)  38,783,897,600 bytes free
